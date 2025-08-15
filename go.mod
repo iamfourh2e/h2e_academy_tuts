3a3f26bd7a0911b4f309d47fb824c8bc2b2ce147
iamfourh2e <iamfourh2e@gmail.com>
2025-08-15 22:25:10 +0700

    first commit for h2e academy school

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_tuts
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+func main() {
+	//data type
+	//int, int32, int64, float32, float64,
+	// string, bool,byte, date,slice
+	// var a int = 10
+	// b := 20
+	// //- + * / % ++ -- **
+	// fmt.Printf("a + b = %d", a+b)
+	// var name string = "Reaksmey Kevin"
+	// replacedName := strings.ReplaceAll(name, "Kevin", "Thkeam")
+	// fmt.Printf("Hello, %s", replacedName)
+
+	// arr := []int{1, 2, 3, 4, 5} // growable array
+	// //arr.push(6)
+	// //arr.add(6)
+
+	// arr = append(arr, 6)
+	// for _, v := range arr {
+	// 	println(v)
+	// }
+	// arr := make([]int, 5) // create an array with length 5
+	// arr[0] = 1
+	// arr[1] = 2
+	// arr[2] = 3
+	// arr[3] = 4
+	// arr[4] = 5
+	//PointerA a x01234
+	//reused variable
+	var a int = 20
+	var b = &a
+	*b = 30
+	// var c = b
+	print(a)
+
+}
+
+//TEST SUIT
