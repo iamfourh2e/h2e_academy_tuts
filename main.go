ba57538243bb48d0f523b2e20934f07f481bcaa1
iamfourh2e <iamfourh2e@gmail.com>
2025-08-22 22:21:43 +0700

    added api

--- main.go
+++ main.go
@@ -1,76 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-type Product struct {
-	Name  string
-	Price float32
-	Qty   float32
+	"github.com/gin-gonic/gin"
+)
+
+type Student struct {
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name"`
+	Age  int32  `json:"age"`
 }
 
-type Order struct {
-	ID           string
-	Items        []Product
-	Discount     float32
-	DiscountType string
-	Subtotal     float32
-	Total        float32
-}
+var Database []Student = []Student{}
 
-func (o *Order) subtotal() {
-	var subtotalItem float32 = 0
-	for _, item := range o.Items {
-		subtotalItem += item.Price * item.Qty
-	}
-	o.Subtotal = subtotalItem
-}
-func (o *Order) total() {
-	var afterDiscount float32 = 0
-	if o.DiscountType == "percentage" {
-		afterDiscount = o.Subtotal - (o.Subtotal * (o.Discount / 100))
-	} else {
-		afterDiscount = o.Subtotal - o.Discount
-	}
-	o.Total = afterDiscount
-}
-
-func (o *Order) display() {
-	str := "----items----\n"
-	for i, item := range o.Items {
-		str += fmt.Sprintf("%d.%s %2.f x %2.f=%2.f\n", i+1, item.Name, item.Price, item.Qty, item.Price*item.Qty)
-	}
-	str += fmt.Sprintf("Subtotal: %2.f\n", o.Subtotal)
-	str += "-------------\n"
-	str += fmt.Sprintf("Discount: %2.f\n", o.Discount)
-	str += fmt.Sprintf("DiscountType: %s\n", o.DiscountType)
-	str += "-------------\n "
-	str += fmt.Sprintf("Total: %2.f", o.Total)
-	fmt.Println(str)
-
-}
 func main() {
-	products := []Product{
-		{
-			Name:  "Mi Cheat",
-			Price: 4000,
-			Qty:   10,
-		},
-		{
-			Name:  "ABC",
-			Price: 4500,
-			Qty:   1,
-		},
-	}
-	order := &Order{
-		ID:           "1",
-		Items:        products,
-		Discount:     10,
-		DiscountType: "percentage",
-		Subtotal:     0,
-		Total:        0,
-	}
-	order.subtotal()
-	order.total()
-	order.display()
+	r := gin.Default()
+	//POST
+	r.POST("/v1/students", func(ctx *gin.Context) {
+		student := &Student{}
+		err := ctx.ShouldBind(&student)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		Database = append(Database, *student)
+		ctx.JSON(http.StatusOK, gin.H{"message": "Student created successfully", "data": student})
+	})
+	r.GET("/v1/students/:id", func(ctx *gin.Context) {
+		id := ctx.Param("id")
+		for _, student := range Database {
+			if student.ID == id {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Student found", "data": student})
+				return
+			}
+		}
+		ctx.JSON(http.StatusNotFound, gin.H{"message": "Student not found"})
+	})
+	r.PUT("/v1/students/:id", func(ctx *gin.Context) {
+		id := ctx.Param("id")                   // retrieve student id
+		studentToUpdate := &Student{}           //declare new student data
+		err := ctx.ShouldBind(&studentToUpdate) // apply data to student object
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"message": "invalid request"})
+			return
+		}
+		for i, student := range Database {
+			if student.ID == id {
+				studentToUpdate.ID = id
+				Database[i] = *studentToUpdate
+				ctx.JSON(http.StatusOK, gin.H{"message": "Student updated successfully", "data": studentToUpdate})
+				return
+			}
+		}
+		ctx.JSON(http.StatusNotFound, gin.H{"message": "Student not found"})
+	})
 
+	r.GET("/v1/students", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK,
+			gin.H{"message": "Students fetched successfully", "data": Database})
+	})
+	r.DELETE("/v1/students/:id", func(ctx *gin.Context) {
+		//5 elements 0 -> 4
+		// numbers := []int{1, 2, 3, 4, 5}
+		// numbers = append(numbers[:2], numbers[3:]...)
+		// fmt.Printf("%v", numbers)
+		id := ctx.Param("id")
+		for i, student := range Database {
+			if student.ID == id {
+				Database = append(Database[:i], Database[i+1:]...)
+				ctx.JSON(http.StatusOK, gin.H{"message": "Student deleted successfully"})
+				return
+			}
+		}
+		ctx.JSON(http.StatusNotFound, gin.H{"message": "Student not found"})
+	})
+	r.Run(":4000")
 }
